f4a9e20858542208f007ca8a0c0eb32f7d7c4528
nasuda <nasuda908@gmail.com>
2024-01-18 21:06:46 +0900

    add js scripts

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ function updateName() {
   const name = prompt('Hey name!');
   para.textContent = `Player 1: ${name}`
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  function createParagraph() {
+    const para = document.createElement('p');
+    para.textContent = 'push Button';
+    document.body.appendChild(para);
+  }
+
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    button.addEventListener('click', createParagraph);
+  }
+});
