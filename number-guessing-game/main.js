a1b79c7784751d0a643ead722c0cda443c52fa16
nasuda <nasuda908@gmail.com>
2024-01-22 21:59:13 +0900

    add js setup

--- number-guessing-game/main.js
+++ number-guessing-game/main.js
@@ -1,7 +1,17 @@
-//alert('hello')
-
-
-console.log('main.js')
-
 let randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+
+const guesses = document.querySelector('.guesses');
+console.log(guesses);
+const lastResult = document.querySelector('.lastResult');
+console.log(lastResult);
+const lowOrHi = document.querySelector('.lowOrHi');
+console.log(lowOrHi);
+
+
+const guessSubmit = document.querySelector('.guessSubmit');
+console.log(guessSubmit);
+const guessField = document.querySelector('.guessField');
+console.log(guessField);
+
+let guessCount = 1;
+let resetButton;
